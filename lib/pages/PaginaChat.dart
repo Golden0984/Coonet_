0b754c36490890830145d9dcd60234df334fcc46
Arnaucj11 <91552395+Arnaucj11@users.noreply.github.com>
2022-05-05 22:28:03 +0200

    Update PaginaChat.dart

--- lib/pages/PaginaChat.dart
+++ lib/pages/PaginaChat.dart
@@ -1,4 +1,3 @@
-
 import 'package:coonet/pages/PaginaTienda.dart';
 import 'package:flutter/material.dart';
 import 'package:glass/glass.dart';
@@ -6,90 +5,104 @@ import 'package:glass/glass.dart';
 import 'PaginaChatPersonal.dart';
 import 'PaginaRegistro.dart';
 
-class PaginaChat extends StatelessWidget{
-
-@override
-  Widget build(BuildContext context){
-    
+class PaginaChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      
-       body: Container (decoration: const BoxDecoration(
+      body: Container(
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('images/fondo.png'),
             fit: BoxFit.cover,
           ),
         ),
         child: ListView(
-        children: 
-            [
-              
-          const Padding(padding: EdgeInsets.only(left: 20, top: 30, bottom: 5), child: Text("MENSAJES", style: TextStyle(fontFamily: 'Arial', fontSize: 30, color: Colors.white),),),
-         
-              ofertas2(context),
-              ofertas2(context),
-              ofertas2(context),
-              ofertas2(context),
-              ofertas2(context),
-              ofertas2(context),
-              ofertas2(context),
-              ofertas2(context),
-              ofertas2(context),
-              ofertas2(context),
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                        size: 35,
 
-             
-          ],),
+                        //TODO ir a pagina chat pero con el bottom menu
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => PaginaChat()));
+                      },
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Text(
+                      "MENSAJES",
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ofertas2(context),
+            ofertas2(context),
+            ofertas2(context),
+            ofertas2(context),
+            ofertas2(context),
+            ofertas2(context),
+            ofertas2(context),
+            ofertas2(context),
+            ofertas2(context),
+            ofertas2(context),
+          ],
+        ),
       ),
     );
   }
 
-
-  Widget ofertas2(BuildContext context){
-    
-    return  Card(
+  Widget ofertas2(BuildContext context) {
+    return Card(
       color: Color.fromARGB(171, 255, 255, 255),
 
       //TODO cambiar card por container
       child: InkWell(
-    onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) =>  testChatInt()));
-           
-    },
-  
-    child: Padding(
-      
-      padding: EdgeInsets.all(10),
-      
-        child: Column(  
-          
-          mainAxisSize: MainAxisSize.min,  
-          children: <Widget>[
-            Row(
-              children: [
-              CircleAvatar(
-                radius: 30.0,
-                child: ClipOval(
-                  child: Image.network('https://upload.wikimedia.org/wikipedia/commons/c/cf/Payaso_Cucharita.jpg',
-                    fit: BoxFit.cover,
-                    width: 100,
-                    height: 100,
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => testChatInt()));
+        },
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 30.0,
+                    child: ClipOval(
+                      child: Image.network(
+                        'https://upload.wikimedia.org/wikipedia/commons/c/cf/Payaso_Cucharita.jpg',
+                        fit: BoxFit.cover,
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
                   ),
-                  ),
-              ),
-              SizedBox(width: 20),
-                Text("Sahil", style: TextStyle(
-                    fontSize: 20.0,
-                  )
-                ),
-              ],
-            )
-            
-          ],  
-        
-        ), 
-    ),
+                  SizedBox(width: 20),
+                  Text("Sahil",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      )),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
-  );
-  
+    );
   }
 }
-
