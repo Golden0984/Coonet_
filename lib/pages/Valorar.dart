39cbe2c452897ea51429b9f3593e70d4cda8e041
Arnaucj11 <91552395+Arnaucj11@users.noreply.github.com>
2022-05-17 19:22:37 +0200

    Update Valorar.dart

--- lib/pages/Valorar.dart
+++ lib/pages/Valorar.dart
@@ -1,4 +1,5 @@
 import 'package:coonet/pages/Menu.dart';
+import 'package:coonet/pages/imagen.dart';
 import 'package:flutter/material.dart';
 
 class ValoWidget extends StatefulWidget {
@@ -8,141 +9,46 @@ class ValoWidget extends StatefulWidget {
 
 class _PagoWidgetState extends State<ValoWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
+  final List<Widget> _paginas = [
+    ValoWidget(),
+    ValoWidget(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: Color(0xFFF1F4F8),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/fondo.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          scrollDirection: Axis.vertical,
-          children: [
-            const SizedBox(height: 62.0),
-            Container(
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
-                        size: 35,
-                      ),
-                      onPressed: () {
-                        paginaActual = 4;
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Menu()));
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: 20,
-                    ),
-                    child: Text(
-                      "VALORAR",
-                      style: TextStyle(
-                          fontFamily: 'Arial',
-                          fontSize: 30,
-                          color: Colors.white),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            ListView(
-              padding: EdgeInsets.zero,
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              children: [
-                _Pagos(),
-                const SizedBox(
-                  height: 10,
-                ),
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Text("Pendientes de Valorar")),
+                Tab(icon: Text("Valorados")),
               ],
             ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _Pagos() {
-    return Padding(
-      padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 12),
-      child: Container(
-        width: 100,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 5,
-              color: Color(0x3B000000),
-              offset: Offset(0, 2),
-            )
-          ],
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Padding(
-          padding: EdgeInsetsDirectional.fromSTEB(12, 12, 12, 12),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'COONET PAGINA WEB RESPONSIVE',
-                    ),
-                  ],
-                ),
+            title: Text("VALORAR"),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_back),
+            ),
+          ),
+          body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/fondo.png'),
+                fit: BoxFit.cover,
               ),
-              Text(
-                '\300€',
-              ),
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'ESTADO: PAGADO.',
-                    ),
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 4, 0),
-                          child: Text(
-                            'Total',
-                            textAlign: TextAlign.end,
-                          ),
-                        ),
-                        Text(
-                          '\300€',
-                          textAlign: TextAlign.end,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
+            ),
+            child: TabBarView(
+              children: [
+               Tab(icon: Text("Pendientes de Valorar")),
+               Tab(icon: Text("Valorados")),
+              ],
+            ),
           ),
         ),
       ),
