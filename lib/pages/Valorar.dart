783e306015178d7798edceb36adba8674b18528c
Arnaucj11 <91552395+Arnaucj11@users.noreply.github.com>
2022-05-17 23:29:33 +0200

    Update Valorar.dart

--- lib/pages/Valorar.dart
+++ lib/pages/Valorar.dart
@@ -1,18 +1,80 @@
+import 'dart:convert';
+
 import 'package:coonet/pages/Menu.dart';
 import 'package:coonet/pages/imagen.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'MisProyectos.dart';
+import 'ServicioWidget.dart';
+import 'Users/Anuncios.dart';
+import 'Users/FreeLancer.dart';
+import 'Users/InfoAnuncio.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'ServicioWidget.dart';
+import 'Users/Anuncios.dart';
+import 'Users/InfoAnuncio.dart';
 
 class ValoWidget extends StatefulWidget {
   @override
   _PagoWidgetState createState() => _PagoWidgetState();
 }
 
+//https://www.youtube.com/watch?v=RKAjUlQgR6A
+//https://www.youtube.com/watch?v=8x2Ssf5OxQ4
+
 class _PagoWidgetState extends State<ValoWidget> {
+  late TextEditingController searchFieldController;
   final scaffoldKey = GlobalKey<ScaffoldState>();
   final List<Widget> _paginas = [
     ValoWidget(),
     ValoWidget(),
   ];
+
+  List<Anuncios> data = <Anuncios>[];
+
+  Future<List<Anuncios>> get_anuncios() async {
+    var data = {"email": login};
+    var url = Uri.parse(
+        'https://phpninjahosting.com/manish/Coonet/Php/Misproyectos.php');
+    final response = await http.post(url, body: data);
+    var datos = jsonDecode(response.body);
+    var registros = <Anuncios>[];
+    if (response.statusCode == 200) {
+      if (datos == 'no') {
+        Fluttertoast.showToast(
+            msg: "No hay anuncios creados", toastLength: Toast.LENGTH_SHORT);
+      } else {
+        for (datos in datos) {
+          registros.add(Anuncios.fromJson(datos));
+        }
+      }
+    } else {
+      // Si la llamada no fue exitosa, lanza un error.
+      throw Exception('Failed to load post');
+    }
+
+    return registros;
+    /*if (response.statusCode == 200) {
+    // Si la llamada al servidor fue exitosa, analiza el JSON
+    
+  } else {
+    // Si la llamada no fue exitosa, lanza un error.
+    throw Exception('Failed to load post');
+  }*/
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    get_anuncios().then((value) {
+      setState(() {
+        data.addAll(value);
+      });
+    });
+    searchFieldController = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +94,11 @@ class _PagoWidgetState extends State<ValoWidget> {
             ),
             title: Text("VALORAR"),
             leading: IconButton(
-              onPressed: () {},
+              onPressed: () {
+                paginaActual = 4;
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Menu()));
+              },
               icon: Icon(Icons.arrow_back),
             ),
           ),
@@ -45,13 +111,129 @@ class _PagoWidgetState extends State<ValoWidget> {
             ),
             child: TabBarView(
               children: [
-               Tab(icon: Text("Pendientes de Valorar")),
-               Tab(icon: Text("Valorados")),
+                MisProyectos(),
+                Tab(icon: Text("Valorados")),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _Anuncio() {
+    return ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return Container(
+            child: Column(
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    ListView(
+                      padding: EdgeInsets.zero,
+                      primary: false,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsetsDirectional.fromSTEB(
+                              16, 8, 16, 0),
+                          child: Container(
+                            width: double.infinity,
+                            height: 200,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: Image.network(
+                                  data[index].foto1,
+                                ).image,
+                              ),
+                              boxShadow: const [
+                                BoxShadow(
+                                  blurRadius: 3,
+                                  color: Color(0x33000000),
+                                  offset: Offset(0, 2),
+                                )
+                              ],
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsetsDirectional.fromSTEB(
+                                  0, 120, 0, 0),
+                              child: Container(
+                                width: 100,
+                                height: 100,
+                                decoration: const BoxDecoration(
+                                  color: Color(0x80FFFFFF),
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(8),
+                                    bottomRight: Radius.circular(8),
+                                    topLeft: Radius.circular(0),
+                                    topRight: Radius.circular(0),
+                                  ),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsetsDirectional.fromSTEB(
+                                      16, 0, 16, 0),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: [
+                                      Expanded(
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.max,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              data[index].titulo,
+                                            ),
+                                            Text(
+                                              data[index].nombre,
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Column(
+                                        mainAxisSize: MainAxisSize.max,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          ElevatedButton(
+                                            onPressed: () {
+                                              String id =
+                                                  data[index].id_anuncio;
+                                              fetchINFO(id);
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          ServicioWidget(
+                                                              free: fetchINFO(
+                                                                  id))));
+                                            },
+                                            child: const Text('Editar'),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
