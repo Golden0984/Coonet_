6c742e4cdf0becc2e52f6cc10bba68daa5b63852
Arnaucj11 <91552395+Arnaucj11@users.noreply.github.com>
2022-05-18 16:14:09 +0200

    Update Valorar.dart

--- lib/pages/Valorar.dart
+++ lib/pages/Valorar.dart
@@ -33,62 +33,28 @@ class _PagoWidgetState extends State<ValoWidget> {
     ValoWidget(),
     ValoWidget(),
   ];
-
-  List<Anuncios> data = <Anuncios>[];
-
-  Future<List<Anuncios>> get_anuncios() async {
-    var data = {"email": login};
-    var url = Uri.parse(
-        'https://phpninjahosting.com/manish/Coonet/Php/Misproyectos.php');
-    final response = await http.post(url, body: data);
-    var datos = jsonDecode(response.body);
-    var registros = <Anuncios>[];
-    if (response.statusCode == 200) {
-      if (datos == 'no') {
-        Fluttertoast.showToast(
-            msg: "No hay anuncios creados", toastLength: Toast.LENGTH_SHORT);
-      } else {
-        for (datos in datos) {
-          registros.add(Anuncios.fromJson(datos));
-        }
-      }
-    } else {
-      // Si la llamada no fue exitosa, lanza un error.
-      throw Exception('Failed to load post');
-    }
-
-    return registros;
-    /*if (response.statusCode == 200) {
-    // Si la llamada al servidor fue exitosa, analiza el JSON
-    
-  } else {
-    // Si la llamada no fue exitosa, lanza un error.
-    throw Exception('Failed to load post');
-  }*/
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    get_anuncios().then((value) {
-      setState(() {
-        data.addAll(value);
-      });
-    });
-    searchFieldController = TextEditingController();
-  }
+  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        tabBarTheme: TabBarTheme(
+          indicator: UnderlineTabIndicator(
+            borderSide: BorderSide(
+              color: Color.fromARGB(255, 255, 255, 255),
+            )
+          )
+        )
+      ),
       home: DefaultTabController(
         length: 2,
         child: Scaffold(
-          extendBodyBehindAppBar: true,
           appBar: AppBar(
-            backgroundColor: Colors.transparent,
+            backgroundColor: Color.fromARGB(255, 138, 3, 228),
             elevation: 0,
             bottom: const TabBar(
+              
               tabs: [
                 Tab(icon: Text("Pendientes de Valorar")),
                 Tab(icon: Text("Valorados")),
@@ -123,119 +89,5 @@ class _PagoWidgetState extends State<ValoWidget> {
     );
   }
 
-  Widget _Anuncio() {
-    return ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (context, index) {
-          return Container(
-            child: Column(
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    ListView(
-                      padding: EdgeInsets.zero,
-                      primary: false,
-                      shrinkWrap: true,
-                      scrollDirection: Axis.vertical,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsetsDirectional.fromSTEB(
-                              16, 8, 16, 0),
-                          child: Container(
-                            width: double.infinity,
-                            height: 200,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: Image.network(
-                                  data[index].foto1,
-                                ).image,
-                              ),
-                              boxShadow: const [
-                                BoxShadow(
-                                  blurRadius: 3,
-                                  color: Color(0x33000000),
-                                  offset: Offset(0, 2),
-                                )
-                              ],
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsetsDirectional.fromSTEB(
-                                  0, 120, 0, 0),
-                              child: Container(
-                                width: 100,
-                                height: 100,
-                                decoration: const BoxDecoration(
-                                  color: Color(0x80FFFFFF),
-                                  borderRadius: BorderRadius.only(
-                                    bottomLeft: Radius.circular(8),
-                                    bottomRight: Radius.circular(8),
-                                    topLeft: Radius.circular(0),
-                                    topRight: Radius.circular(0),
-                                  ),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsetsDirectional.fromSTEB(
-                                      16, 0, 16, 0),
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.max,
-                                    children: [
-                                      Expanded(
-                                        child: Column(
-                                          mainAxisSize: MainAxisSize.max,
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              data[index].titulo,
-                                            ),
-                                            Text(
-                                              data[index].nombre,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      Column(
-                                        mainAxisSize: MainAxisSize.max,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          ElevatedButton(
-                                            onPressed: () {
-                                              String id =
-                                                  data[index].id_anuncio;
-                                              fetchINFO(id);
-                                              Navigator.push(
-                                                  context,
-                                                  MaterialPageRoute(
-                                                      builder: (context) =>
-                                                          ServicioWidget(
-                                                              free: fetchINFO(
-                                                                  id))));
-                                            },
-                                            child: const Text('Editar'),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          );
-        });
-  }
+  
 }
